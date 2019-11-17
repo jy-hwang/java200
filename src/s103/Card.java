55d9edbf18109a39f615a0985aa191563e98886c
strat <strat@DESKTOP-8C299G9>
2019-11-17 14:35:09 +0900

    java200

---
+++ src/s103/Card.java
@@ -0,0 +1,29 @@
+package s103;
+
+public class Card {
+
+	private String cardVal;
+
+	public String getCardVal() {
+		return cardVal;
+	}
+
+	public Card() {
+		this("H2");
+		// this.cardVal = "H2"
+	}
+
+	public Card(String s) {
+		this.cardVal = s;
+	}
+
+	public Card(Card c) {
+		this(c.getCardVal());
+	}
+
+	@Override
+	public String toString() {
+		return "[" + cardVal + "]";
+	}
+
+}
---
+++ src/s104/Card.java
@@ -0,0 +1,22 @@
+package s104;
+
+public class Card {
+
+	
+	private String cardVal;
+	
+	
+	public String getCardVal() {
+		return cardVal;
+		
+		
+	}
+	
+	public Card() {
+		
+		
+		
+	}
+	
+	
+}
